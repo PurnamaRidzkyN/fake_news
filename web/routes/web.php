a66bcd6c014db73653ef4a5394a1ccc1d776d1cd
mrfauzii <reishifauzi24@gmail.com>
2026-04-21 17:22:38 +0700

    Implement WhatsApp login functionality with token verification and update user model

--- web/routes/web.php
+++ web/routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RiwayatController;
 use App\Http\Controllers\WaController;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('landing_page.landing');
@@ -24,13 +26,21 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
 });
 
 Route::any('/wa-webhook', [WaController::class, 'webhook']);
+Route::post('/detect-hoax', [ApiController::class, 'detectHoax']);
+
+//login menggunakan wa di web
+Route::get('/login-wa', [AuthController::class, 'showPhoneForm']);
+Route::post('/login-wa/request', [AuthController::class, 'requestToken']);
+Route::get('/login-wa/verify', [AuthController::class, 'showTokenForm']);
+Route::post('/login-wa/verify', [AuthController::class, 'verifyToken']);
+
+
 
 // Route khusus untuk user yang sudah login di Web
 Route::middleware(['auth'])->group(function () {
-    
+
     // ... (taruh route dashboard lu di sini nanti kalau ada)
-    
+
     // Route buat nyambungin WA
     Route::post('/link-wa', [WaController::class, 'linkWhatsApp'])->name('wa.link');
-
 });
